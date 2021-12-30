806583b2d24ec4864d88f407d2f26fb57aca5de9
aaneloy <neloyn@myumanitoba.ca>
2021-12-29 21:19:01 -0600

    closes #2

---
+++ data-scaler/data-scaler.py
@@ -0,0 +1,155 @@
+import warnings
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import RobustScaler
+
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.svm import SVR
+
+warnings.filterwarnings("ignore")
+
+
+def rmse(predictions, targets):
+    return np.sqrt(((predictions - targets) ** 2).mean())
+
+
+def r2_score(y, y_hat):
+    y_bar = y.mean()
+    ss_tot = ((y - y_bar) ** 2).sum()
+    ss_res = ((y - y_hat) ** 2).sum()
+    return 1 - (ss_res / ss_tot)
+
+def scaler(X,y):
+    # split into train and test sets
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
+    # summarize
+    print('Train Size', X_train.shape, y_train.shape)
+    print('Test Size', X_test.shape, y_test.shape)
+
+    col_list = list(X.columns.values)
+
+    # fit scaler on training data
+    norm = MinMaxScaler().fit(X_train)
+
+    # transform training data
+    X_train_norm = norm.transform(X_train)
+
+    # transform testing dataabs
+    X_test_norm = norm.transform(X_test)
+
+    # copy of datasets
+    X_train_stand = X_train.copy()
+    X_test_stand = X_test.copy()
+
+    # numerical features
+
+    # apply standardization on numerical features
+    for i in col_list:
+        # fit on training data column
+        scale = StandardScaler().fit(X_train_stand[[i]])
+
+        # transform the training data column
+        X_train_stand[i] = scale.transform(X_train_stand[[i]])
+
+        # transform the testing data column
+        X_test_stand[i] = scale.transform(X_test_stand[[i]])
+
+    # copy of datasets
+    X_train_rob = X_train.copy()
+    X_test_rob = X_test.copy()
+
+    # numerical features
+
+    # apply standardization on numerical features
+    for i in col_list:
+        # fit on training data column
+        scale = RobustScaler().fit(X_train_rob[[i]])
+
+        # transform the training data column
+        X_train_rob[i] = scale.transform(X_train_rob[[i]])
+
+        # transform the testing data column
+        X_test_rob[i] = scale.transform(X_test_rob[[i]])
+
+    LR = LinearRegression()
+
+    rmse = []
+    r2 = []
+
+    # raw, normalized and standardized training and testing data
+    trainX = [X_train, X_train_norm, X_train_stand, X_train_rob]
+    testX = [X_test, X_test_norm, X_test_stand, X_test_rob]
+
+    # model fitting and measuring RMSE
+    for i in range(len(trainX)):
+        # fit
+        LR.fit(trainX[i], y_train)
+        # predict
+        pred = LR.predict(testX[i])
+        # RMSE
+        rmse.append(rmse(y_test, pred))
+        # r2
+        r2.append(r2_score(y_test, pred))
+
+    # visualizing the result
+    df_LR = pd.DataFrame({'RMSE': rmse, 'R2': r2, 'Model': LR}, index=['Original', 'Normalized', 'Standardized', 'RobustScaler'])
+    print(df_LR)
+
+    # rfr = RandomForestRegressor(n_estimators = 100, max_depth = 5, min_samples_leaf= 5, max_features = 'sqrt')  # using GridSearch
+    rfr = RandomForestRegressor()
+
+    rmse = []
+    r2 = []
+
+    # raw, normalized and standardized training and testing data
+    trainX = [X_train, X_train_norm, X_train_stand, X_train_rob]
+    testX = [X_test, X_test_norm, X_test_stand, X_test_rob]
+
+    # model fitting and measuring RMSE
+    for i in range(len(trainX)):
+        # fit
+        rfr.fit(trainX[i], y_train)
+        # predict
+        pred = rfr.predict(testX[i])
+        # RMSE
+        rmse.append(rmse(y_test, pred))
+        # R2
+        r2.append(r2_score(y_test, pred))
+
+    # visualizing the result
+    df_rf = pd.DataFrame({'RMSE': rmse, 'R2': r2, 'Model': rfr},
+                          index=['Original', 'Normalized', 'Standardized', 'RobustScaler'])
+    print(df_rf)
+
+    svr = SVR(kernel='rbf')
+
+    rmse = []
+    r2 = []
+
+    # raw, normalized and standardized training and testing data
+    trainX = [X_train, X_train_norm, X_train_stand, X_train_rob]
+    testX = [X_test, X_test_norm, X_test_stand, X_test_rob]
+
+    # model fitting and measuring RMSE
+    for i in range(len(trainX)):
+        # fit
+        svr.fit(trainX[i], y_train)
+        # predict
+        pred = svr.predict(testX[i])
+        # RMSE
+        rmse.append(rmse(y_test, pred))
+        # R2
+        r2.append(r2_score(y_test, pred))
+
+    # visualizing the result
+    df_svr = pd.DataFrame({'RMSE': rmse, 'R2': r2, 'Model': svr},
+                          index=['Original', 'Normalized', 'Standardized', 'RobustScaler'])
+    print(df_svr)
+
+
--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@ import setuptools
 
 
 setuptools.setup(
-
+    name='data_inspector',
+    version='1.0',
+    author="Asif Ahmed Neloy",
+    author_email="neloyn@myumanitoba.ca",
+    description="Data Scaler is an open-source python library to select the appropriate data scaler for your Machine Learning model.",
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    License='MIT',
+    long_description_content_type="text/markdown",
+    url="https://github.com/aaneloy/data-scaler",
+    keywords='scaler',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[]
+    install_requires=[['pandas==1.2.3','matplotlib==3.1.2','numpy==1.18.5', 'seaborn==0.11.1','scipy==1.6.2']]
 )
